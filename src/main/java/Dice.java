ce7e64c53d3b3807e0dfe6ec581635e62385739a
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-08 14:15:36 -0500

    created and passed numOfDice test

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,26 @@
 public class Dice {
+    Integer faceValue;
+    Integer summedFaceValue;
+    Integer numOfDice;
 
+    public Dice(Integer numOfDice) {
+        this.numOfDice = numOfDice;
+    }
 
+    public Integer tossAndSum(Integer numOfDice){
+       summedFaceValue = 0;
+        for (int i = 0; i<numOfDice; i++){
+            faceValue = (int)(Math.random() * (6 - 1)) + 1;
+            summedFaceValue += faceValue;
+        }
+        return summedFaceValue;
+    }
+
+    public Integer getNumOfDice(){
+        return numOfDice;
+    }
+
+    public void setNumOfDice(Integer numOfDice){
+        this.numOfDice = numOfDice;
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,9 @@
 public class Simulation {
 
+    //Simulation sim = new Simulation(2, 10000);
 
+    public void runSimulation(){}
+
+    public void printResults(){}
 
 }
